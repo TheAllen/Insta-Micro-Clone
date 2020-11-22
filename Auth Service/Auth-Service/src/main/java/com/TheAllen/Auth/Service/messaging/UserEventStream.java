561895e7344581f19205ec10d40497c805392138
TheAllen <allenlee1995@hotmail.com>
2020-11-21 22:53:34 -0700

    created a Stream Messaging

--- Auth Service/Auth-Service/src/main/java/com/TheAllen/Auth/Service/messaging/UserEventStream.java
+++ Auth Service/Auth-Service/src/main/java/com/TheAllen/Auth/Service/messaging/UserEventStream.java
@@ -1,4 +1,12 @@
 package com.TheAllen.Auth.Service.messaging;
 
-public class UserEventStream {
+import org.springframework.cloud.stream.annotation.Output;
+import org.springframework.messaging.MessageChannel;
+
+public interface UserEventStream {
+
+    String OUTPUT = "momentsUserChanged";
+
+    @Output(value = OUTPUT)
+    MessageChannel momentsUserChanged();
 }
