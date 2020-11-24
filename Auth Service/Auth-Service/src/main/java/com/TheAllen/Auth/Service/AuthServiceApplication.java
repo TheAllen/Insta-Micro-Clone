3d382a72a9677635ea829572860be5695f251d71
TheAllen <allenlee1995@hotmail.com>
2020-11-24 01:44:09 -0700

    added JwtConfig

--- Auth Service/Auth-Service/src/main/java/com/TheAllen/Auth/Service/AuthServiceApplication.java
+++ Auth Service/Auth-Service/src/main/java/com/TheAllen/Auth/Service/AuthServiceApplication.java
@@ -4,11 +4,14 @@ import com.TheAllen.Auth.Service.messaging.UserEventStream;
 import com.TheAllen.Auth.Service.models.Role;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.Output;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.kafka.annotation.EnableKafkaStreams;
 
 @SpringBootApplication
 @EnableMongoAuditing
+@EnableKafkaStreams
 @EnableBinding(UserEventStream.class)
 public class AuthServiceApplication {
 
