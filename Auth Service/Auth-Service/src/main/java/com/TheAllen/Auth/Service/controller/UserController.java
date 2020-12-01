ff755d8c5f0294ea11fdf204366229f85eb52e16
TheAllen <allenlee1995@hotmail.com>
2020-11-30 23:43:53 -0700

    added a auth endpoint

--- Auth Service/Auth-Service/src/main/java/com/TheAllen/Auth/Service/controller/UserController.java
+++ Auth Service/Auth-Service/src/main/java/com/TheAllen/Auth/Service/controller/UserController.java
@@ -1,18 +1,27 @@
 package com.TheAllen.Auth.Service.controller;
 
 import com.TheAllen.Auth.Service.service.JwtProvider;
+import com.TheAllen.Auth.Service.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
+@RequestMapping(value = "/api")
 public class UserController {
 
     Logger logger = LoggerFactory.getLogger(UserController.class);
 
     @Autowired
-    private UserService
+    private UserService userService;
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String hello() {
+        return "Authentication Server";
+    }
 }
