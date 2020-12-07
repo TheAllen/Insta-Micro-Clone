a974270a698b00616cc1589708890111b40cfcbc
TheAllen <allenlee1995@hotmail.com>
2020-12-06 23:34:06 -0700

    worked on Authentication filter

--- Auth Service/Auth-Service/src/main/java/com/TheAllen/Auth/Service/config/JwtAuthenticationFilter.java
+++ Auth Service/Auth-Service/src/main/java/com/TheAllen/Auth/Service/config/JwtAuthenticationFilter.java
@@ -2,7 +2,11 @@ package com.TheAllen.Auth.Service.config;
 
 import com.TheAllen.Auth.Service.service.JwtProvider;
 import com.TheAllen.Auth.Service.service.UserService;
+import com.ctc.wstx.util.StringUtil;
+import io.jsonwebtoken.Claims;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -40,6 +44,20 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         if(header == null || !header.startsWith(jwtConfig.getPrefix())) {
             filterChain.doFilter(httpServletRequest, httpServletResponse);
             return;
+        }
+
+        //Retrieve token
+        //TODO get the jwt from the cookies
+        String token = httpServletRequest.getHeader(jwtConfig.getPrefix());
+
+        if(StringUtils.hasText(token) && jwtProvider.validateToken(token)) {
+            Claims claims = jwtProvider.getClaimsFromJWT(token);
+            String username = claims.getSubject();
+
+            UsernamePasswordAuthenticationToken
+        } else { //Token validated
+
+
         }
 
     }
