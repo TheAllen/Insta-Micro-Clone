fac10bd073bd880657d48c91b08d16cfedaa62f8
TheAllen <allenlee1995@hotmail.com>
2020-12-11 22:02:47 -0700

    implemented sign in endpoint

--- Auth Service/Auth-Service/src/main/java/com/TheAllen/Auth/Service/controller/UserController.java
+++ Auth Service/Auth-Service/src/main/java/com/TheAllen/Auth/Service/controller/UserController.java
@@ -1,14 +1,25 @@
 package com.TheAllen.Auth.Service.controller;
 
+import com.TheAllen.Auth.Service.payload.JwtAuthenticationResponse;
+import com.TheAllen.Auth.Service.payload.LoginRequest;
 import com.TheAllen.Auth.Service.service.JwtProvider;
 import com.TheAllen.Auth.Service.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @Slf4j
@@ -20,8 +31,36 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtProvider jwtProvider;
+
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String hello() {
         return "Authentication Server";
     }
+
+    //Sign in
+    @RequestMapping(value = "/signin", method = RequestMethod.POST)
+    public ResponseEntity<?> userSignIn(@Valid @RequestBody LoginRequest loginRequest) {
+
+        //Authentication
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginRequest.getUsername(),
+                        loginRequest.getPassword()
+                )
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        String jwt = jwtProvider.generateToken(authentication);
+
+        return ResponseEntity.ok(new JwtAuthenticationResponse(jwt));
+    }
+
+    //Register
+
 }
