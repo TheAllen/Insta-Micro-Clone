7a1358f1b1b8f5d3e6b75f9c42b2e4bde6396f32
TheAllen <allenlee1995@hotmail.com>
2020-12-11 21:51:22 -0700

    implemented InstaUserDetailsService

---
+++ Auth Service/Auth-Service/src/main/java/com/TheAllen/Auth/Service/service/InstaUserDetailsService.java
@@ -0,0 +1,29 @@
+package com.TheAllen.Auth.Service.service;
+
+import com.TheAllen.Auth.Service.models.InstaUserDetails;
+import com.TheAllen.Auth.Service.models.User;
+import com.TheAllen.Auth.Service.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class InstaUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserService userService;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        return userService
+                .findByUsername(username)
+                .map(InstaUserDetails::new)
+                .orElseThrow(() -> new UsernameNotFoundException("Username not found"));
+    }
+
+}
