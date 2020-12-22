5c2033c34c86690aed355ae8d534bb7b0d89a98d
TheAllen <allenlee1995@hotmail.com>
2020-12-21 22:59:54 -0700

    added authenticationManager

--- Auth Service/Auth-Service/src/main/java/com/TheAllen/Auth/Service/config/SecurityConfig.java
+++ Auth Service/Auth-Service/src/main/java/com/TheAllen/Auth/Service/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -78,7 +79,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
     }
 
-
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
 
 
 }
