4045657293880dcbf6512d3a908169805e6fb2c5
TheAllen <allenlee1995@hotmail.com>
2021-01-04 09:24:45 -0700

    added JwtAuthenticationFilter

---
+++ media-service/media-service/src/main/java/com/TheAllen/mediaservice/configuration/JwtAuthenticationFilter.java
@@ -0,0 +1,25 @@
+package com.TheAllen.mediaservice.configuration;
+
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtConfig jwtConfig;
+
+    public JwtAuthenticationFilter(JwtConfig jwtConfig) {
+        this.jwtConfig = jwtConfig;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain)
+            throws ServletException, IOException {
+
+
+    }
+}
