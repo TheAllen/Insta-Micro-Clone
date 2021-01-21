d17ac1a78fb4cb5151716bab4a7d1a15cfe9d85a
TheAllen <allenlee1995@hotmail.com>
2021-01-20 21:30:35 -0700

    added StaticResource config

--- media-service/media-service/src/main/java/com/TheAllen/mediaservice/service/FileStorageService.java
+++ media-service/media-service/src/main/java/com/TheAllen/mediaservice/service/FileStorageService.java
@@ -1,7 +1,28 @@
 package com.TheAllen.mediaservice.service;
 
+import com.TheAllen.mediaservice.model.ImageMetadata;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
+@Slf4j
 public class FileStorageService {
+
+    @Value()
+    private String uploadDirectory;
+
+    @Value()
+    private String filePathPrefix;
+
+    @Autowired
+    private Environment environment;
+
+    public ImageMetadata store(MultipartFile file, String username) {
+
+        String filename
+    }
 }
