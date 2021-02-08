cc9d2d095055891ea89742b2fbe6c0cea71ca77d
TheAllen <allenlee1995@hotmail.com>
2021-02-08 13:39:07 -0700

    added security token config

---
+++ media-service/media-service/src/main/java/com/TheAllen/mediaservice/configuration/SecurityTokenConfig.java
@@ -0,0 +1,37 @@
+package com.TheAllen.mediaservice.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.http.HttpServletResponse;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityTokenConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private JwtConfig jwtConfig;
+
+    @Value("file.path.prefix")
+    private String filePathPrefix;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) //Stateless sessions
+                .and()
+                .exceptionHandling().authenticationEntryPoint((req, res, e) -> res.sendError(HttpServletResponse.SC_UNAUTHORIZED))
+                .and()
+                .addFilterAfter(new JwtAuthenticationFilter(jwtConfig), UsernamePasswordAuthenticationFilter.class)
+                .authorizeRequests()
+                .antMatchers(filePathPrefix + "/**").permitAll()
+                .anyRequest().authenticated();
+    }
+}
