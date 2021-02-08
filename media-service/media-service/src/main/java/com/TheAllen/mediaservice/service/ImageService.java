d9a53dd28ba51f0870f0414663cbc53584d5c6fe
TheAllen <allenlee1995@hotmail.com>
2021-02-08 14:10:19 -0700

    implemented addImage method in ImageService

--- media-service/media-service/src/main/java/com/TheAllen/mediaservice/service/ImageService.java
+++ media-service/media-service/src/main/java/com/TheAllen/mediaservice/service/ImageService.java
@@ -2,11 +2,14 @@ package com.TheAllen.mediaservice.service;
 
 import com.TheAllen.mediaservice.model.ImageMetadata;
 import com.TheAllen.mediaservice.repository.ImageMetadataRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 @Service
+@Slf4j
 public class ImageService {
 
     @Autowired
@@ -17,5 +20,14 @@ public class ImageService {
 
     public ImageMetadata addImage(MultipartFile file, String username) {
 
+        //Get filename from file
+        String filename = StringUtils.cleanPath(file.getOriginalFilename());
+
+        //Log message
+
+        //pass in file and username
+        ImageMetadata metadata = fileStorageService.store(file, username);
+
+        return imageMetadataRepository.save(metadata);
     }
 }
